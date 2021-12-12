a74d6d8ee8df8e478065bc6e76d7a1e52ce72e61
Hariom <harim987316@gmail.com>
2021-12-12 14:39:25 +0530

    formatting file

--- dependencies/indexJavaScript.js
+++ dependencies/indexJavaScript.js
@@ -4,17 +4,17 @@
  */
 //  for Vertical bar Navigation
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 // Get the element with id="defaultOpen" and click on it
@@ -27,9 +27,9 @@ console.log("Width----->", w);
 console.log("height----->", h);
 
 function myFunction() {
-    var cw = window.innerWidth;
-    var ch = window.innerHeight;
-    console.log("Change Width------>", cw);
-    console.log("Change Height------>", ch);
+  var cw = window.innerWidth;
+  var ch = window.innerHeight;
+  console.log("Change Width------>", cw);
+  console.log("Change Height------>", ch);
 }
 //Copyright @ 2021 Hariom Gola. All Right Reserved Not to be Used for Making profit
